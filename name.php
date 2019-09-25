61a1c1a69633154ca36bb63b5d7d405eb3511377
unknown <2120367@talnet.nl>
2019-09-25 11:38:56 +0200

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,18 @@
+<?php
+
+$naam;
+$achternaam;
+
+echo 'Wat is je naam? ' . "\n";
+
+$naam = readline();
+
+echo 'Wat is je achternaam? ' . "\n";
+
+$achternaam = readline();
+
+echo "Hallo " . $naam .  ' ' . $achternaam;
+
+
+
+ ?>
